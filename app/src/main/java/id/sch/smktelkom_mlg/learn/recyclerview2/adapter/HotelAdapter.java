9c52f3225908000af6c51a4e09590f29d735b3e0
Nirmala Ainun <nirmala.ainun.na@gmail.com>
2016-12-14 05:37:15 +0700

    Initial Commit RecyclerView2

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/adapter/HotelAdapter.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.learn.recyclerview2.adapter;
+
+/**
+ * Created by Nirmala Ainun on 14/12/2016.
+ */
+
+public class HotelAdapter {
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/model/Hotel.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.learn.recyclerview2.model;
+
+/**
+ * Created by Nirmala Ainun on 14/12/2016.
+ */
+
+public class Hotel {
+}
